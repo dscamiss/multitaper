99543f0e63163dbfc8287d6cdaba049a5be4c6e7
Scott <dscamiss@gmail.com>
2025-03-26 01:46:26 -0700

    Update docstring, add version

--- src/multitaper/__init__.py
+++ src/multitaper/__init__.py
@@ -1,4 +1,11 @@
+"""
+The multitaper package computes multitaper spectrum estimates for univariate
+time series.  Additionally, the package computes cross-spectrum, coherence,
+and transfer function estimates for bivariate time series.
+"""
+
 from src.multitaper.mtcross import MTCross, SineCross
 from src.multitaper.mtspec import MTSine, MTSpec
 
 __all__ = ["MTCross", "SineCross", "MTSine", "MTSpec"]
+__version__ = "1.2.0"
